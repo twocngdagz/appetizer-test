188b5456bc4d97d62539444c092f84a226627ef8
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-02-23 00:53:42 +0800

    add routes for Passer Resources

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Api'], function () {
+
+    Route::resource('passers', 'PasserController', [
+        'except' => [
+            'update', 'edit', 'show', 'destroy', 'create'
+        ]
+    ]);
 });
