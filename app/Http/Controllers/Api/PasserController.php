d6dd4b1df4e49a93ef4d3a4e2cc3c32724051739
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-02-24 23:53:58 +0800

    add backend functionality for adding passer

--- app/Http/Controllers/Api/PasserController.php
+++ app/Http/Controllers/Api/PasserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Requests\Api\CreatePasser;
 use App\Passer;
 use App\Services\Filters\PasserFilter;
 use App\Services\Paginate\Paginate;
@@ -20,5 +21,17 @@ class PasserController extends ApiController
         $passers = new Paginate(Passer::filter($filter));
 
         return $this->respondWithPagination($passers);
+    }
+
+    public function store(CreatePasser $request)
+    {
+        $passer = Passer::create([
+            'name' => $request->input('passer.name'),
+            'school' => $request->input('passer.school'),
+            'campus' => $request->input('passer.campus'),
+            'division' => $request->input('passer.division')
+        ]);
+
+        return $this->respondWithTransformer($passer);
     }
 }
